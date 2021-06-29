fc68e93cbcc16e7795ee5de23d8a6f3914236418
garyMamaniCortez <mamanijhonny2210@gmail.com>
2021-06-29 10:20:18 -0400

    feat: created clientes components and json

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,9 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+    {id: 1,nombre: 'gary', apellido: 'mamani', email: 'gary@gmail', createAt: '2021-06-14'},
+    {id: 2,nombre: 'rodrigo', apellido: 'peralta', email: 'aldo@gmail', createAt: '2021-06-14'},
+    {id: 3,nombre: 'aldo', apellido: 'lima', email: 'aldo@gmail', createAt: '2021-06-14'},
+    {id: 4,nombre: 'jose', apellido: 'quiroga', email: 'jose@gmail', createAt: '2021-06-14'},
+    {id: 5,nombre: 'luz', apellido: 'soliz', email: 'luz@gmail', createAt: '2021-06-14'}
+  ];
