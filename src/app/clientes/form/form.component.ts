0ab7799a712f0ca1a1c5e3d5b58d25c90a036fe2
garyMamaniCortez <mamanijhonny2210@gmail.com>
2021-07-02 12:25:44 -0400

    feat: created function create()

--- src/app/clientes/form/form.component.ts
+++ src/app/clientes/form/form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from '../cliente';
+import { ClienteService } from '../cliente.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-form',
@@ -10,14 +12,16 @@ export class FormComponent implements OnInit {
   cliente: Cliente = new Cliente()
   titulo: string = "Crear Cliente"
 
-  constructor() { }
+  constructor(private clienteService: ClienteService,
+    private router: Router) { }
 
   ngOnInit(): void {
   }
 
   public create(): void{
-    console.log("clicked")
-    console.log(this.cliente)
+    this.clienteService.create(this.cliente).subscribe(
+      response => this.router.navigate(['/clientes'])
+    )
   }
 }
 //componente creado con ng g c clientes/form
