3224ed55d28be3d9289fa06e5a696037efb3381c
garyMamaniCortez <mamanijhonny2210@gmail.com>
2021-07-02 12:34:43 -0400

    feat: sweetalert2 alert new client

--- src/app/clientes/form/form.component.ts
+++ src/app/clientes/form/form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Cliente } from '../cliente';
 import { ClienteService } from '../cliente.service';
 import { Router } from '@angular/router';
+import swal from 'sweetalert2'
 
 @Component({
   selector: 'app-form',
@@ -20,8 +21,11 @@ export class FormComponent implements OnInit {
 
   public create(): void{
     this.clienteService.create(this.cliente).subscribe(
-      response => this.router.navigate(['/clientes'])
-    )
+      response => {
+        this.router.navigate(['/clientes'])
+        swal.fire('Nuevo cliente', `Cliente ${this.cliente.nombre} creado con exito`, 'success')
+      }
+    );
   }
 }
 //componente creado con ng g c clientes/form
