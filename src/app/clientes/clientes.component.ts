80bec842da925135e335ef9436f50be4588103e0
garyMamaniCortez <mamanijhonny2210@gmail.com>
2021-07-05 11:29:10 -0400

    feat: create sweet alert to delete cliente

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-clientes',
@@ -18,5 +19,44 @@ export class ClientesComponent implements OnInit {
       clientes => this.clientes = clientes
     );
   }
-
+  delete(cliente: Cliente): void{
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: 'btn btn-success',
+        cancelButton: 'btn btn-danger'
+      },
+      buttonsStyling: false
+    })
+    
+    swalWithBootstrapButtons.fire({
+      title: 'Estas seguro?',
+      text: `No se podra revertir el cliente ${cliente.nombre}`,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'si, eliminar',
+      cancelButtonText: 'No, cancelar',
+      reverseButtons: true
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.clienteService.delete(cliente.id).subscribe(
+          response => {
+            this.clientes = this.clientes.filter(cli => cli!== cliente)
+            swalWithBootstrapButtons.fire(
+              'Eliminado',
+              `El cliente: ${cliente.nombre} fue eliminado`,
+              'success'
+            )
+          }
+        )
+      } else if (
+        result.dismiss === Swal.DismissReason.cancel
+      ) {
+        swalWithBootstrapButtons.fire(
+          'Cancelado',
+          'No se elimino el cliente',
+          'error'
+        )
+      }
+    })
+  }
 }
